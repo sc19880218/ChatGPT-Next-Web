e6a575990b8525c3f5f5f2edd40fbe67f7fa92b9
lisq10 <lisq10@asiainfo.com>
2024-08-09 13:07:29 +0800

    1

--- app/constant.ts
+++ app/constant.ts
@@ -222,6 +222,7 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 
 const openaiModels = [
   "gpt-3.5-turbo",
+  "gpt-3.5-turbo-0301",
   "gpt-3.5-turbo-1106",
   "gpt-3.5-turbo-0125",
   "gpt-4",
